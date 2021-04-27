48081bcb3521996cf3dccbadcf2156318d813d4e
Miguel Medellín <mamedellinr@unal.edu.co>
2021-04-27 11:54:20 -0500

    update script

--- lua_scripts/channels.lua
+++ lua_scripts/channels.lua
@@ -1,5 +1,7 @@
-local function OnPlayerCommands(evento, jugador, comando)
-    jugador:SendAreaTriggerMessage("Mi primer Script en LUA")
+local PLAYER_EVENT_ON_LOGIN = 3
+
+local function OnLogin(event, player)
+    player:SendBroadcastMessage("Hello world")
 end
 
-RegisterPlayerEvent(42, OnPlayerCommands)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, OnLogin)
