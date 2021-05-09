c1897785f19e9c04d1ecd1debb42410d0c2c99cf
Miguel Medellín <mamedellinr@unal.edu.co>
2021-05-08 19:38:03 -0500

    add core fixes

--- lua_scripts/world_chat.lua
+++ lua_scripts/world_chat.lua
@@ -1,10 +1,10 @@
 local PLAYER_EVENT_ON_LOGIN = 3
-local PLAYER_EVENT_ON_CHANNEL_CHAT = 22;
+local PLAYER_EVENT_ON_CHANNEL_CHAT = 22
 
-local channelName = "Lithium";
-local channelId = 1;
-local duration = 5; -- in seconds.
-local WorldChannelChat = {};
+local channelName = "Lithium"
+local channelId = 1
+local duration = 5 -- in seconds
+local WorldChannelChat = {}
 
 local colors = { -- colors for names and misc
     [0] = "|cff3399FF", -- Color for Alliance Name
@@ -14,28 +14,28 @@ local colors = { -- colors for names and misc
     [4] = "|cff7030A0", -- Color for GM level 1: Moderator
     [5] = "|cffCC00FF", -- Color for GM level 2: Gamemaster
     [6] = "|cffF400A1" -- Color for GM level 3: Administrator
-};
+}
 
 local gmRank = {
     [0] = "jugador",
     [1] = "moderador",
     [2] = "GM",
     [3] = "admin"
-};
+}
 
 local function Login(event, player)
-    player:SendBroadcastMessage("Usa /1 o /3 para conversar en Canal Global Lithium");
+    player:SendBroadcastMessage("Usa /join Lithium para unirte al Chat Global, usa /4 para conversar.")
 end
 
 local function ChatSystem(event, player, msg, Type, lang, channel)
-    local id = player:GetGUIDLow();
+    local id = player:GetGUIDLow()
 
     if not (WorldChannelChat[id]) then
         WorldChannelChat[id] = {
             time = GetGameTime() - duration,
             lastMessage = "",
             isGmEnabled = false
-        };
+        }
     end
 
     if (channel == channelId) then
@@ -44,43 +44,43 @@ local function ChatSystem(event, player, msg, Type, lang, channel)
                 if (msg ~= "Away") then
                     if (player:GetGMRank() > 0 and WorldChannelChat[id].isGmEnabled) then
                         if (msg == ".gm off") then
-                            WorldChannelChat[id].isGmEnabled = false;
-                            player:SendBroadcastMessage("Modo GM en Canal Global desactivado.");
+                            WorldChannelChat[id].isGmEnabled = false
+                            player:SendBroadcastMessage("Modo GM en Canal Global desactivado.")
                         elseif (string.sub(msg, 1, 1) == ".") then
-                            player:SendBroadcastMessage("Estás en canal global, no te deja usar comandos GM.");
+                            player:SendBroadcastMessage("Estás en canal global, no te deja usar comandos GM.")
                         else
                             local t = table.concat {colors[player:GetGMRank() + 3], "[", channelName, "][",
-                                                    player:GetName(), "][", gmRank[player:GetGMRank()], "]: ", msg, "|r"};
-                            SendWorldMessage(t);
+                                                    player:GetName(), "][", gmRank[player:GetGMRank()], "]: ", msg, "|r"}
+                            SendWorldMessage(t)
                         end
                     else
                         if (player:GetGMRank() > 0 and msg == ".gm on") then
-                            WorldChannelChat[id].isGmEnabled = true;
+                            WorldChannelChat[id].isGmEnabled = true
                             player:SendBroadcastMessage("Modo GM en Canal Global activado.");
-                            return false;
+                            return false
                         end
 
-                        local time = GetGameTime();
+                        local time = GetGameTime()
                         if (msg ~= WorldChannelChat[id].lastMessage) then
                             if ((time - WorldChannelChat[id].time >= duration)) then
                                 local t = table.concat {colors[2], "[", channelName, "]|r", colors[player:GetTeam()],
                                                         "[", "|Hplayer:", player:GetName(), "|h", player:GetName(),
-                                                        "|h][", player:GetLevel(), "]|r", colors[2], ": ", msg, "|r"};
-                                SendWorldMessage(t);
-                                WorldChannelChat[id].time = time;
-                                WorldChannelChat[id].lastMessage = msg;
+                                                        "|h][", player:GetLevel(), "]|r", colors[2], ": ", msg, "|r"}
+                                SendWorldMessage(t)
+                                WorldChannelChat[id].time = time
+                                WorldChannelChat[id].lastMessage = msg
                             else
                                 player:SendBroadcastMessage(colors[6] ..
-                                                                "Se activó el temporizador de spam para el chat global.|r");
+                                                                "Se activó el temporizador de spam para el chat global.|r")
                             end
                         else
-                            player:SendBroadcastMessage(colors[6] .. "Se detectó spam en el chat global.|r");
+                            player:SendBroadcastMessage(colors[6] .. "Se detectó spam en el chat global.|r")
                         end
                     end
                 end
             end
         end
-        return false;
+        return false
     end
 end
 
